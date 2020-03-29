603f2de3eded307218c32ab895ff0441d1aef8e4
dariocurreri <dariocurrerimobile@gmail.com>
2020-03-29 19:18:28 +0200

    fill db

--- scripts/fill_db.py
+++ scripts/fill_db.py
@@ -1,1 +1,11 @@
 # https://pypi.org/project/python-codicefiscale/, libreria per generare cf
+import os
+import numpy as np
+import pandas as pd
+
+names = pd.read_csv("../res/datasets/nomi.csv")
+surnames = pd.read_csv("../res/datasets/cognomi.csv")
+
+for i in range(0, 100):
+    print(names.iloc[np.random.randint(0, 100), 0] + " " +
+          surnames.iloc[np.random.randint(0, 100), 0])
