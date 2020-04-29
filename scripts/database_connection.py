a5551f9a8f07ad9c504d02a885da353da15de55d
dariocurreri <dariocurr@gmail.com>
2020-04-29 13:04:44 +0200

    update database connection

--- scripts/database_connection.py
+++ scripts/database_connection.py
@@ -1,21 +1,23 @@
 import pymysql.cursors
 
-connection = pymysql.connect(host='bdm.zandes.net',
-                             port=3306,
-                             user='bdm_root',
-                             password='progettobdm',
-                             db='bdm_uni_riconciliato')
 
+class database_connection():
 
-def execute_query(query):
-    try:
-        with connection.cursor() as sql:
-            sql.execute(query)
-            result = sql.fetchall()
-            if result is not None:
-                for row in result:
-                    print(row)
-            else:
-                connection.commit()
-    finally:
-        connection.close()
+    def __init__(self, database):
+        self._connection = pymysql.connect(host='bdm.zandes.net',
+                                           port=3306,
+                                           user='bdm_root',
+                                           password='progettobdm',
+                                           db=database)
+
+    def execute_query(self, query):
+        try:
+            with self._connection.cursor() as sql:
+                sql.execute(query)
+                result = sql.fetchall()
+                if result is not None:
+                    return result
+                else:
+                    self._connection.commit()
+        finally:
+            self._connection.close()
