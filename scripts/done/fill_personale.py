97ca17355ddd76140cd5091ca02c5d8a3375a888
dariocurreri <dariocurr@gmail.com>
2020-05-02 12:27:35 +0200

    creazione tutti tipi personale

--- scripts/done/fill_personale.py
+++ scripts/done/fill_personale.py
@@ -24,7 +24,7 @@ def creazione_personale():
     for db in ["bdm_unipa", "bdm_unina", "bdm_unito", "bdm_unimi"]:
         sql = database_connection(db)
         dipartimenti = list(sql.execute_query("SELECT id_dipartimento, nome FROM dipartimento"))
-        for i in range(0, 200):
+        for i in range(0, 500):
             name = names.iloc[np.random.randint(0, 100), 0]
             name = name[0] + str(name[1:]).lower()
             surname = surnames.iloc[np.random.randint(0, 100), 0]
@@ -46,102 +46,116 @@ def creazione_personale():
                 query += "'" + str(value) + "',"
             sql.execute_query(query[:-1] + ")")
 
-    """
-    prefisso_telefono_unipa = "091 238 "
-    prefisso_telefono_unito = "011 670 "
-    prefisso_telefono_unina = "081 420 "
-    prefisso_telefono_unimi = "091 03 "
+
+def creazione_personale_specifico():
+    # ta: livello, stipendio, qualifica, telefono, anagrafica_personale
+    # ricerca: tipologia_contratto, stipendio, anagrafica_personale, indice_qualita
+    # non struttrato: laboratorio_riferimento, telefono, personale_ricerca
+    # struttrato: telefono, ufficio, personale_ricerca
     livello_stipendio_qualifica = list()
     livello_stipendio_qualifica.append(("B", 1050.45, "Titolo di studio di scuola d’obbligo"))
     livello_stipendio_qualifica.append(("C", 1231.97, "Diploma di scuola superiore"))
     livello_stipendio_qualifica.append(("D", 1767.81, "Diploma di laurea"))
     livello_stipendio_qualifica.append(("EP", 2083.33, "Diploma di laurea e particolare qualificazione personale"))
     tipologia_contratto_strutturato = list()
-    tipologia_contratto_strutturato.append("Professore ordinario")
-    tipologia_contratto_strutturato.append("Professore associato")
-    tipologia_contratto_strutturato.append("Ricercatore a tempo indeterminato")
-    tipologia_contratto_strutturato.append("Ricercatore a tempo determinato")
+    tipologia_contratto_strutturato.append(("Professore ordinario", 2550.72))
+    tipologia_contratto_strutturato.append(("Professore associato", 2123.19))
+    tipologia_contratto_strutturato.append(("Ricercatore a tempo indeterminato", 1802.89))
+    tipologia_contratto_strutturato.append(("Ricercatore a tempo determinato", 1478.11))
     tipologia_contratto_non_strutturato = list()
-    tipologia_contratto_non_strutturato.append("Assegnista")
-    tipologia_contratto_non_strutturato.append("Dottorando")
-    tipologia_contratto_non_strutturato.append("Contrattista")
-    #GENERAZIONE PERSONALE
-        telefono = prefisso_telefono_unipa
-        #telefono = prefisso_telefono_unito
-        #telefono = prefisso_telefono_unina
-        #telefono = prefisso_telefono_unimi
-        for _ in range(0, 4):
-            telefono += str(np.random.randint(0, 10))
-        stipendio = (2020 - int(date[:4])) * 6.34
-        random_number = np.random.randint(1, 4)
-        #PERSONALE TA
-        if random_number == 1:
-            random_number = np.random.randint(1, 11)
-            livello = ""
-            qualifica = ""
-            if random_number < 4:
-                livello = livello_stipendio_qualifica[0][0]
-                stipendio += livello_stipendio_qualifica[0][1]
-                qualifica = livello_stipendio_qualifica[0][2]
-            elif random_number < 8:
-                livello = livello_stipendio_qualifica[1][0]
-                stipendio += livello_stipendio_qualifica[1][1]
-                qualifica = livello_stipendio_qualifica[1][2]
-            elif random_number < 10:
-                livello = livello_stipendio_qualifica[2][0]
-                stipendio += livello_stipendio_qualifica[2][1]
-                qualifica = livello_stipendio_qualifica[2][2]
-            else:
-                livello = livello_stipendio_qualifica[3][0]
-                stipendio += livello_stipendio_qualifica[3][1]
-                qualifica = livello_stipendio_qualifica[3][2]
-        else:
-            tipologia_contratto = ""
-            stipendio = np.nan
-            #PERSONALE STRUTTURATO
-            if random_number == 2:
-                random_number = np.random.randint(1, 8)
-                promozioni = list()
-                if random_number < 2:
-                    tipologia_contratto = tipologia_contratto_strutturato[0]
-                    stipendio += 2550.72
-                    prima_promozione_anno_inizio = np.random.randint(1980, 2000)
-                    prima_promozione_anno_fine = np.random.randint(prima_promozione_anno_inizio, 2005)
-                    vecchio_stipendio = (prima_promozione_anno_fine - int(date[:4])) * 6.34 + 1478.11
-                    promozioni.append((str(prima_promozione_anno_inizio) + "/01/01", str(prima_promozione_anno_fine) + "/31/12", vecchio_stipendio, tipologia_contratto_strutturato[3]))
-                    seconda_promozione_anno_inizio = np.random.randint(prima_promozione_anno_fine, 2010)
-                    seconda_promozione_anno_fine = np.random.randint(seconda_promozione_anno_inizio, 2015)
-                    vecchio_stipendio = (seconda_promozione_anno_fine - int(date[:4])) * 6.34 + 1802.89
-                    promozioni.append((str(seconda_promozione_anno_inizio) + "/01/01", str(seconda_promozione_anno_fine) + "/31/12", vecchio_stipendio, tipologia_contratto_strutturato[2]))
-                    terza_promozione_anno_inizio = np.random.randint(seconda_promozione_anno_fine, 2018)
-                    terza_promozione_anno_fine = np.random.randint(terza_promozione_anno_inizio, 2019)
-                    vecchio_stipendio = (terza_promozione_anno_fine - int(date[:4])) * 6.34 + 2123.19
-                    promozioni.append((str(terza_promozione_anno_inizio) + "/01/01", str(terza_promozione_anno_fine) + "/31/12", vecchio_stipendio, tipologia_contratto_strutturato[1]))
-                elif random_number < 4:
-                    tipologia_contratto = tipologia_contratto_strutturato[1]
-                    stipendio += 2123.19
-                    prima_promozione_anno_inizio = np.random.randint(1990, 2010)
-                    prima_promozione_anno_fine = np.random.randint(prima_promozione_anno_inizio, 2015)
-                    vecchio_stipendio = (prima_promozione_anno_fine - int(date[:4])) * 6.34 + 1478.11
-                    promozioni.append((str(prima_promozione_anno_inizio) + "/01/01", str(prima_promozione_anno_fine) + "/31/12", vecchio_stipendio, tipologia_contratto_strutturato[3]))
-                    seconda_promozione_anno_inizio = np.random.randint(prima_promozione_anno_fine, 2018)
-                    seconda_promozione_anno_fine = np.random.randint(seconda_promozione_anno_inizio, 2019)
-                    vecchio_stipendio = (seconda_promozione_anno_fine - int(date[:4])) * 6.34 + 1802.89
-                    promozioni.append((str(seconda_promozione_anno_inizio) + "/01/01", str(seconda_promozione_anno_fine) + "/31/12", vecchio_stipendio, tipologia_contratto_strutturato[2]))
-                elif random_number < 6:
-                    tipologia_contratto = tipologia_contratto_strutturato[2]
-                    stipendio += 1802.89
-                    anno_inizio = np.random.randint(2000, 2018)
-                    anno_fine = np.random.randint(anno_inizio, 2019)
-                    vecchio_stipendio = (anno_fine - int(date[:4])) * 6.34 + 1478.11
-                    promozioni.append((str(anno_inizio) + "/01/01", str(anno_fine) + "/31/12", vecchio_stipendio, tipologia_contratto_strutturato[3]))
+    tipologia_contratto_non_strutturato.append(("Assegnista", 1323.34))
+    tipologia_contratto_non_strutturato.append(("Dottorando", 1231.90))
+    tipologia_contratto_non_strutturato.append(("Contrattista", 1002.43))
+    for db in ["bdm_unipa", "bdm_unina", "bdm_unito", "bdm_unimi"]:
+        sql = database_connection(db)
+        persone = list(sql.execute_query("SELECT cf, data_nascita FROM personale"))
+        prefisso = ""
+        if db == "bdm_unipa":
+            prefisso = "091 238 "
+        elif db == "bdm_unito":
+            prefisso = "011 670 "
+        elif db == "bdm_unina":
+            prefisso = "081 420 "
+        elif db == "bdm_unimi":
+            prefisso = "091 03 "
+        for persona in persone:
+            telefono = prefisso
+            for _ in range(0, 4):
+                telefono += str(np.random.randint(0, 10))
+            stipendio = (2020 - int(str(persona[1])[:4])) * 6.34
+            random_number = np.random.randint(1, 4)
+            if random_number == 1:
+                #PERSONALE TA
+                random_number = np.random.randint(1, 11)
+                livello = ""
+                qualifica = ""
+                if random_number < 4:
+                    livello = livello_stipendio_qualifica[0][0]
+                    stipendio += livello_stipendio_qualifica[0][1]
+                    qualifica = livello_stipendio_qualifica[0][2]
+                elif random_number < 8:
+                    livello = livello_stipendio_qualifica[1][0]
+                    stipendio += livello_stipendio_qualifica[1][1]
+                    qualifica = livello_stipendio_qualifica[1][2]
+                elif random_number < 10:
+                    livello = livello_stipendio_qualifica[2][0]
+                    stipendio += livello_stipendio_qualifica[2][1]
+                    qualifica = livello_stipendio_qualifica[2][2]
                 else:
-                    tipologia_contratto = tipologia_contratto_strutturato[3]
-                    stipendio += 1478.11
-                ufficio = np.random.randint(101, 300)
-            #PERSONALE NON STRUTTURATO
+                    livello = livello_stipendio_qualifica[3][0]
+                    stipendio += livello_stipendio_qualifica[3][1]
+                    qualifica = livello_stipendio_qualifica[3][2]
+                ta = (livello, stipendio, qualifica, telefono, persona[0])
+                query = "INSERT INTO personale_ta(livello, stipendio, qualifica, telefono, anagrafica_personale) VALUES("
+                for value in ta:
+                    query += "'" + str(value) + "',"
+                sql.execute_query(query[:-1] + ")")
             else:
-                laboratorio_riferimento = "Lab " + str(np.random.randint(1, 100))
-    """
-
-creazione_personale()
+                #PERSONALE_RICERCA
+                tipologia_contratto = ""
+                if random_number == 2:
+                    #PERSONALE STRUTTURATO
+                    random_number = np.random.randint(1, 8)
+                    if random_number < 2:
+                        tipologia_contratto = tipologia_contratto_strutturato[0][0]
+                        stipendio += tipologia_contratto_strutturato[0][1]
+                    elif random_number < 4:
+                        tipologia_contratto = tipologia_contratto_strutturato[1][0]
+                        stipendio += tipologia_contratto_strutturato[1][1]
+                    elif random_number < 6:
+                        tipologia_contratto = tipologia_contratto_strutturato[2][0]
+                        stipendio += tipologia_contratto_strutturato[2][1]
+                    else:
+                        tipologia_contratto = tipologia_contratto_strutturato[3][0]
+                        stipendio += tipologia_contratto_strutturato[3][1]
+                    ricerca = (tipologia_contratto, stipendio, persona[0], np.random.randint(1, 11))
+                    query = "INSERT INTO personale_ricerca(tipologia_contratto, stipendio, anagrafica_personale, indice_qualita) VALUES("
+                    for value in ricerca:
+                        query += "'" + str(value) + "',"
+                    sql.execute_query(query[:-1] + ")")
+                    ufficio = np.random.randint(101, 300)
+                    query = "SELECT id_personale_ricerca, stipendio FROM personale_ricerca WHERE anagrafica_personale = \"" + str(persona[0]) + "\""
+                    result = list(sql.execute_query(query))
+                    strutturato = (telefono, ufficio, result[0][0])
+                    query = "INSERT INTO personale_strutturato(telefono, ufficio, personale_ricerca) VALUES("
+                    for value in strutturato:
+                        query += "'" + str(value) + "',"
+                    sql.execute_query(query[:-1] + ")")
+                else:
+                    #PERSONALE NON STRUTTURATO
+                    random = np.random.randint(0, 3)
+                    tipologia_contratto = tipologia_contratto_non_strutturato[random][0]
+                    stipendio += tipologia_contratto_non_strutturato[random][1]
+                    ricerca = (tipologia_contratto, stipendio, persona[0], np.random.randint(1, 11))
+                    query = "INSERT INTO personale_ricerca(tipologia_contratto, stipendio, anagrafica_personale, indice_qualita) VALUES("
+                    for value in ricerca:
+                        query += "'" + str(value) + "',"
+                    sql.execute_query(query[:-1] + ")")
+                    laboratorio_riferimento = "Lab " + str(np.random.randint(1, 100))
+                    query = "SELECT id_personale_ricerca, stipendio FROM personale_ricerca WHERE anagrafica_personale = \"" + str(persona[0]) + "\""
+                    result = list(sql.execute_query(query))
+                    non_strutturato = (laboratorio_riferimento, telefono, result[0][0])
+                    query = "INSERT INTO personale_non_strutturato(laboratorio_riferimento, telefono, personale_ricerca) VALUES("
+                    for value in non_strutturato:
+                        query += "'" + str(value) + "',"
+                    sql.execute_query(query[:-1] + ")")
