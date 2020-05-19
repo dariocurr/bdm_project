b0eb30e2fb75003025de8ee87f8a07e7aa1a8ec4
dariocurreri <dariocurr@gmail.com>
2020-05-19 17:20:03 +0200

    update

--- scripts/fill_db_riconciliato.py
+++ scripts/fill_db_riconciliato.py
@@ -1,12 +1,12 @@
 from database_connection import database_connection
 
 
-sql_uni = database_connection("bdm_uni_riconciliato")
 id_uni = 1
 offset = 0
 for db in ["bdm_unipa", "bdm_unimi", "bdm_unito", "bdm_unina"]:
     db += "_backup"
     sql = database_connection(db)
+    sql_uni = database_connection("bdm_uni_riconciliato")
     tables = list(sql.execute_query("SHOW TABLES"))
     for table in tables:
         keys = list(sql.execute_query("DESCRIBE " + str(table[0])))
@@ -21,11 +21,10 @@ for db in ["bdm_unipa", "bdm_unimi", "bdm_unito", "bdm_unina"]:
                 index += 1
             if table[0] == "dipartimento":
                 row = row + [id_uni]
-            if table[0] == "contiene_3":
-                query = "INSERT INTO " + table[0] + " VALUES ("
-                for value in row:
-                    query += "'" + str(value) + "',"
-                sql_uni.execute_query(query[:-1] + ")")
+            query = "INSERT INTO " + table[0] + " VALUES ("
+            for value in row:
+                query += "'" + str(value) + "',"
+            sql_uni.execute_query(query[:-1] + ")")
         print("caricata tabella " + table[0])
     offset += 100000
     id_uni += 1
