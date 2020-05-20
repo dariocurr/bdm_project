a4dae778060e4b572c5087249989e9c8d588ac67
dariocurreri <dariocurr@gmail.com>
2020-05-20 09:47:44 +0200

    update

--- scripts/done/insert_NULL.py
+++ scripts/done/insert_NULL.py
@@ -4,24 +4,19 @@ import math
 
 
 def insert_NULL():
-    for db in ["bdm_unipa", "bdm_unina", "bdm_unito", "bdm_unimi"]:
+    for db in ["bdm_uni_riconciliato"]:
         sql = database_connection(db)
         agenzia_esterna = sql.execute_query("SELECT id_agenzia, nome, descrizione, tipologia, indirizzo FROM agenzia_esterna")
         for i in range(0, math.ceil((len(agenzia_esterna))*5/100)):
             a = random.randint(0, 2)
+            query = ""
             if a == 0:
                 query = "UPDATE agenzia_esterna SET indirizzo = NULL WHERE id_agenzia = {}".format(agenzia_esterna[random.randint(0, len(agenzia_esterna)-1)][0])
-                sql.execute_query(query)
             elif a == 1:
                 query = "UPDATE agenzia_esterna SET descrizione = NULL WHERE id_agenzia = {}".format(agenzia_esterna[random.randint(0, len(agenzia_esterna)-1)][0])
-                sql.execute_query(query)
             elif a == 2:
                 query = "UPDATE agenzia_esterna SET tipologia = NULL WHERE id_agenzia = {}".format(agenzia_esterna[random.randint(0, len(agenzia_esterna)-1)][0])
-                sql.execute_query(query)
-
-
-            print(query)
-
+            sql.execute_query(query)
 
 
 insert_NULL()
